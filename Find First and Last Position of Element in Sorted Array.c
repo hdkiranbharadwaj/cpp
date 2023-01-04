74b37fd54c6afadb5c1483a1b64d7b60a336b03f
hdkiranbharadwaj <104179274+hdkiranbharadwaj@users.noreply.github.com>
2023-01-04 22:12:31 +0530

    Update Find First and Last Position of Element in Sorted Array.c

--- Find First and Last Position of Element in Sorted Array.c
+++ Find First and Last Position of Element in Sorted Array.c
@@ -1,17 +1,4 @@
-int* searchRange(int* nums, int numsSize, int target, int* returnSize){
-int* array=(int*)malloc(2*sizeof(int));
-int end=numsSize-1;
-int start=0;
-int mid;
-while(start<end)
-{
-    mid=(end+start)/2;
-    if(mid==target) break;
-    else if(target>*(nums+mid)) low=mid+1;
-    else end=mid-1;
-}
-    
-}
+
 int Lower(int target,int mid,int* nums)
 {
     if(mid==0||*(nums+mid-1)!=*(nums+mid-1))
@@ -19,6 +6,20 @@ int Lower(int target,int mid,int* nums)
         return mid;
     }
     return Lower (target,mid-1,nums);
+}
+while(mid>-1)
+{
+    if(mid==0||nums[mid-1]!=nums[mid])
+       array[0]=mid;
+    else
+    mid--;
+}
+while(mid<=nums.length)
+{
+    if(mid==nums.length||nums[mid+1]!=nums[mid])
+       array[0]=mid;
+    else
+    mid++;
 }
 int Upper(int target,int mid,int* nums,int n)
 {
